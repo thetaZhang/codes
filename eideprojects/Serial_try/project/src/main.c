9e5fc4a91402e72038bb1ed8b24e7d63116f3a2d
hy_Zhang <Zhy_theta@outlook.com>
2023-07-20 15:37:57 +0800

    fix:修改T0初始化函数中对TMOD寄存器赋值的方式

--- eideprojects/Serial_try/project/src/main.c
+++ eideprojects/Serial_try/project/src/main.c
@@ -9,6 +9,14 @@
 */
 
 #include <reg52.h>
-#include "Timer0.h"
 #include <stdio.h>
 
+/**
+ * @brief UART串口初始化函数，配置SCON、PCON寄存器
+ */
+void UART_init()
+{
+    SCON=0x40;//SM0和SM1为01设置为方式1，REN为0禁止接收
+    PCON=0;
+
+}
