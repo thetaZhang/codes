e111ffc8d6888fec0179153f0e76c3117bbb734e
hy_Zhang <Zhy_theta@outlook.com>
2023-07-26 16:35:16 +0800

    🦄 refactor:

---
+++ eideprojects/template/project/src/main.c
@@ -0,0 +1,38 @@
+/*************************************
+ * 
+ *        89c52 sdcc blink demo
+ * 
+ * CPU: 89C52
+ * FREQ: 12MHz
+ * PIN: P00
+ * 
+ * ***********************************
+*/
+
+#include <reg52.h>
+#include <stdio.h>
+
+void delay_100ms(); //@12MHz
+
+void main()
+{
+    while (1)
+    {
+        P00 = !P00;
+        delay_100ms();
+    }
+}
+
+void delay_100ms()
+{
+    unsigned char i, j;
+
+    i = 195;
+    j = 138;
+
+    do
+    {
+        while (--j)
+            ;
+    } while (--i);
+}
