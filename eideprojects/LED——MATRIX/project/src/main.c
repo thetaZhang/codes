a1985d4b269b1e5ebe03adc925a407a50a78c292
hy_Zhang <Zhy_theta@outlook.com>
2023-07-28 00:25:31 +0800

    ✨ feat:

--- eideprojects/LED——MATRIX/project/src/main.c
+++ eideprojects/LED——MATRIX/project/src/main.c
@@ -13,13 +13,25 @@
 #include <74HC595.h>
 
 
+/**
+ * @brief  点阵按列显示函数，选取一列（从0开始编号），按输入数据显示
+ * 
+ * @param Column 选择显示的列数
+ * @param Date 显示的二进制数据
+ */
+void MatrixLED_ShowColumn(unsigned char Column,unsigned char Date)
+{
+    _74HC595_WriteByte(Date);
+    P0=~(0x80>>Column);
+}
+
 
 
 
 void main()
 {
     _74HC595_Init();
-    _74HC595_WriteByte(0x66);
+    MatrixLED_ShowColumn(0,0xAA);
     while (1){
 
     }
