a7d1f81af724d0fa44dde440489f07efbe5172d4
hy_Zhang <Zhy_theta@outlook.com>
2023-07-28 13:38:07 +0800

    🦄 refactor:

--- eideprojects/DS1302clk/project/src/main.c
+++ eideprojects/DS1302clk/project/src/main.c
@@ -1,6 +1,6 @@
 /*************************************
  * 
- *        89c52 sdcc 
+ *        89c52 sdcc DS1302 CLK
  * 
  * CPU: 89C52
  * FREQ: 12MHz
@@ -10,14 +10,17 @@
 
 #include <reg52.h>
 #include <stdio.h>
+#include <LCD1602.h>
+
 
 void main()
 {
+    LCD_Init();
+    LCD_ShowNum(1,1,1,1);
+    
     while (1)
     {
-
+      
     }
 }
 
-
-}
