17f6d3994a734e290a77f89d19736896025d9f96
hyZhang <Zhy_theta@outlook.com>
2023-09-08 22:10:47 +0800

    🦄 refactor:

--- eideprojects/DS1302clk/project/src/main.c
+++ eideprojects/DS1302clk/project/src/main.c
@@ -13,11 +13,11 @@
 #include <LCD1602.h>
 #include <DS1302.h>
 #include <Delay.h>
-#include <Timer0.h>
 
 
-unsigned char Second,second_1,second_10,Minute,minute_1,minute_10,Hour,hour_1,hour_10;
-unsigned char Date,date_1,date_10,Month,month_1,month_10,Year,year_1,year_10;
+
+unsigned char Second,Minute,Hour;
+unsigned char Date,Month,Year;
 char* weekday[7]={"Mon", "Tue", "Wed", "Thu","Fri", "Sat", "Sun"};
 unsigned int state=0;//显示模式，0为时间，1为日期
 
@@ -31,22 +31,16 @@ void ShowTime()
     LCD_ShowChar(2,6,':');
 
     Second=DS1302_ReadByte(0x81);
-    second_1=Second&0x0F;
-    LCD_ShowNum(2,8,second_1,1);
-    second_10=(Second>>4)&0x07;
-    LCD_ShowNum(2,7,second_10,1);   //DS1302存时间是BCD码
+    Second=(Second/16)*10+Second%16; //BCD码转十进制
+    LCD_ShowNum(2,7,Second,2);
         
     Minute=DS1302_ReadByte(0x83);
-    minute_1=Minute&0x0F;
-    LCD_ShowNum(2,5,minute_1,1);
-    minute_10=(Minute>>4)&0x07;
-    LCD_ShowNum(2,4,minute_10,1);
+    Minute=(Minute/16)*10+Minute%16;
+    LCD_ShowNum(2,4,Minute,2);
         
     Hour=DS1302_ReadByte(0x85);
-    hour_1=Hour&0x0F;
-    LCD_ShowNum(2,2,hour_1,1);
-    hour_10=(Hour>>4)&0x03;
-    LCD_ShowNum(2,1,hour_10,1);
+    Hour=(Hour/16)*10+Hour%16;
+    LCD_ShowNum(2,1,Hour,2);
 
     LCD_ShowString(1,1,weekday[DS1302_ReadByte(0x8B)-1]);
 }
@@ -61,50 +55,25 @@ void ShowDate()
     LCD_ShowChar(2,6,'-');
 
     Date=DS1302_ReadByte(0x87);
-    date_1=Date&0x0F;
-    LCD_ShowNum(2,8,date_1,1);
-    date_10=(Date>>4)&0x03;
-    LCD_ShowNum(2,7,date_10,1);
+    Date=(Date/16)*10+Date%16;
+    LCD_ShowNum(2,7,Date,2);
 
     Month=DS1302_ReadByte(0x89);
-    month_1=Month&0x0F;
-    LCD_ShowNum(2,8,month_1,1);
-    month_10=(Month>>4)&0x01;
-    LCD_ShowNum(2,7,month_10,1);
+    Month=(Month/16)*10+Month%16;
+    LCD_ShowNum(2,4,Month,2);
 
     Year=DS1302_ReadByte(0x8D);
-    year_1=Year&0x0F;
-    LCD_ShowNum(2,8,year_1,1);
-    year_10=(Year>>4)&0x0F;
-    LCD_ShowNum(2,7,year_10,1);
+    Year=(Year/16)*10+Year%16;
+    LCD_ShowNum(2,1,Year,2);
 
     LCD_ShowString(1,1,weekday[DS1302_ReadByte(0x8B)-1]); 
 }
-/**
- * @brief 定时器中断处理
- * 
- */
-void Timer0_Routine()  interrupt 1
-{
-    static unsigned int T0Count;//计时器计数
-    TH0=64535/256;
-    TL0=64535%256;
-    T0Count++;
-    if(T0Count>=1000)
-    {
-       //在这里写上中断处理内容
-        
-       //
-        T0Count=0;
-        
-    }
-}
+
 
 void main()
 {
     LCD_Init();
     DS1302_Init();
-    Timer0_Init();
     DS1302_WriteByte(0x80,0x00);
     DS1302_WriteByte(0x82,0x00);
     DS1302_WriteByte(0x84,0x00);
