330e90bb520d92cfdffad1caa4fa946e5b92f262
hyZhang <Zhy_theta@outlook.com>
2023-09-08 23:34:24 +0800

    ✨ feat:

--- eideprojects/DS1302clk/project/src/main.c
+++ eideprojects/DS1302clk/project/src/main.c
@@ -16,10 +16,7 @@
 
 
 
-unsigned char Second,Minute,Hour;
-unsigned char Date,Month,Year;
 char* weekday[7]={"Mon", "Tue", "Wed", "Thu","Fri", "Sat", "Sun"};
-unsigned int state=0;//显示模式，0为时间，1为日期
 
 /**
  * @brief 在LCD上显示当前时间
@@ -27,70 +24,35 @@ unsigned int state=0;//显示模式，0为时间，1为日期
  */
 void ShowTime()
 {
-    LCD_ShowChar(2,3,':');
-    LCD_ShowChar(2,6,':');
+    DS1302_ReadTime();
 
-    Second=DS1302_ReadByte(0x81);
-    Second=(Second/16)*10+Second%16; //BCD码转十进制
-    LCD_ShowNum(2,7,Second,2);
-        
-    Minute=DS1302_ReadByte(0x83);
-    Minute=(Minute/16)*10+Minute%16;
-    LCD_ShowNum(2,4,Minute,2);
-        
-    Hour=DS1302_ReadByte(0x85);
-    Hour=(Hour/16)*10+Hour%16;
-    LCD_ShowNum(2,1,Hour,2);
-
-    LCD_ShowString(1,1,weekday[DS1302_ReadByte(0x8B)-1]);
+    LCD_ShowNum(1,1,DS1302_Time[0],2);
+    LCD_ShowNum(1,4,DS1302_Time[1],2);
+    LCD_ShowNum(1,7,DS1302_Time[2],2);
+    LCD_ShowNum(2,1,DS1302_Time[3],2);
+    LCD_ShowNum(2,4,DS1302_Time[4],2);
+    LCD_ShowNum(2,7,DS1302_Time[5],2);
+    LCD_ShowString(1,14,weekday[DS1302_Time[6]-1]);
 }
 
-/**
- * @brief 在LCD上显示日期
- * 
- */
-void ShowDate()
-{
-    LCD_ShowChar(2,3,'-');
-    LCD_ShowChar(2,6,'-');
 
-    Date=DS1302_ReadByte(0x87);
-    Date=(Date/16)*10+Date%16;
-    LCD_ShowNum(2,7,Date,2);
-
-    Month=DS1302_ReadByte(0x89);
-    Month=(Month/16)*10+Month%16;
-    LCD_ShowNum(2,4,Month,2);
-
-    Year=DS1302_ReadByte(0x8D);
-    Year=(Year/16)*10+Year%16;
-    LCD_ShowNum(2,1,Year,2);
-
-    LCD_ShowString(1,1,weekday[DS1302_ReadByte(0x8B)-1]); 
-}
 
 
 void main()
 {
     LCD_Init();
     DS1302_Init();
-    DS1302_WriteByte(0x80,0x00);
-    DS1302_WriteByte(0x82,0x00);
-    DS1302_WriteByte(0x84,0x00);
-    DS1302_WriteByte(0x86,0x00);
-    DS1302_WriteByte(0x88,0x00);
-    DS1302_WriteByte(0x8C,0x00);
-    DS1302_WriteByte(0x8A,0x01);
+    LCD_ShowChar(2,3,':');
+    LCD_ShowChar(2,6,':');
+    LCD_ShowChar(1,3,'-');
+    LCD_ShowChar(1,6,'-');
+    DS1302_SetTime();
     
 
     while (1)
     {
-        if(state==0){
-            ShowTime();
-        }
-        else {
-            ShowDate();
-        }
+        
+        ShowTime();
         Delay_ms(10);
     }
 }
--- eideprojects/Tools/src/DS1302.c
+++ eideprojects/Tools/src/DS1302.c
@@ -1,18 +1,17 @@
-/**
- * @file DS1302.c
- * @author theta
- * @brief DS1302模块
- * @version 0.1
- * @date 2023-08-24
- * 
- * @copyright Copyright (c) 2023
- * 
- */
-
-
 #include <DS1302.h>
 #include <reg52.h>
 #include <stdio.h>
+
+#define DS1302_SECOND   0x80
+#define DS1302_MINUTE   0x82
+#define DS1302_HOUR     0x84
+#define DS1302_DATE     0x86
+#define DS1302_MONTH    0x88
+#define DS1302_DAY      0x8A
+#define DS1302_YEAR     0x8C
+#define DS1302_WP       0x8E //写保护
+
+unsigned int DS1302_Time[7]={0,0,0,0,0,0,1};   //全局变量，在c中定义，在h中声明为extern，则不会重定义
 
 /**
  * @brief DS1302初始化
@@ -58,6 +57,7 @@ void DS1302_WriteByte(unsigned char Command,unsigned char Data)
 unsigned char DS1302_ReadByte(unsigned char Command)
 {
     DS1302_CE=1;
+    Command|=0x01; //这样只要给写的地址就行了，用最上面的宏定义，给一个写的，这里自动变成对应的读的
     unsigned char Data=0x00;
     for (int i=0;i<8;i++)
     {
@@ -75,4 +75,37 @@ unsigned char DS1302_ReadByte(unsigned char Command)
     DS1302_CE=0;
     DS1302_IO=0;
     return Data;
+}
+
+/**
+ * @brief 设置时间函数
+ * 
+ */
+void DS1302_SetTime()
+{
+    DS1302_WriteByte(DS1302_WP,0x00);//关闭写保护
+    DS1302_WriteByte(DS1302_YEAR,DS1302_Time[0]/10*16+DS1302_Time[0]%10);
+    DS1302_WriteByte(DS1302_MONTH,DS1302_Time[1]/10*16+DS1302_Time[1]%10);
+    DS1302_WriteByte(DS1302_DATE,DS1302_Time[2]/10*16+DS1302_Time[2]%10);
+    DS1302_WriteByte(DS1302_HOUR,DS1302_Time[3]/10*16+DS1302_Time[3]%10);
+    DS1302_WriteByte(DS1302_MINUTE,DS1302_Time[4]/10*16+DS1302_Time[4]%10);
+    DS1302_WriteByte(DS1302_SECOND,DS1302_Time[5]/10*16+DS1302_Time[5]%10);
+    DS1302_WriteByte(DS1302_DAY,DS1302_Time[6]/10*16+DS1302_Time[6]%10);  
+}
+
+/**
+ * @brief 读取时间函数，存储在全局变量Time中
+ * 
+ */
+void DS1302_ReadTime()
+{
+    unsigned char tmp;
+    tmp=DS1302_ReadByte(DS1302_YEAR);DS1302_Time[0]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_MONTH);DS1302_Time[1]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_DATE);DS1302_Time[2]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_HOUR);DS1302_Time[3]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_MINUTE);DS1302_Time[4]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_SECOND);DS1302_Time[5]=tmp/16*10+tmp%16;
+    tmp=DS1302_ReadByte(DS1302_DAY);DS1302_Time[6]=tmp/16*10+tmp%16;
+
 }
